d3663e841ae578d4cc9beeb6e778f3a117fc04d5
Mahmoud Habib <mhmoudrhabib@gmail.com>
2025-05-02 14:00:38 +0300

    Refactor navigation bar and transitions
    - Added surface color at elevation to the navigation bar.
    - Added transition animations for the navigation host.
    
    Signed-off-by: Mahmoud Habib <mhmoudrhabib@gmail.com>

--- app/src/main/java/com/shamela/library/presentation/screens/HomeHostScreen.kt
+++ app/src/main/java/com/shamela/library/presentation/screens/HomeHostScreen.kt
@@ -2,15 +2,20 @@ package com.shamela.library.presentation.screens
 
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -18,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -73,7 +79,9 @@ fun HomeHostScreen() {
                 enter = slideInVertically(initialOffsetY = { it }),
                 exit = slideOutVertically(targetOffsetY = { it }),
             ) {
-                NavigationBar() {
+                NavigationBar(
+                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp),
+                ) {
                     val navBackStackEntry by navController.currentBackStackEntryAsState()
                     val currentDestination = navBackStackEntry?.destination
                     destination.forEachIndexed { index, screen ->
@@ -130,7 +138,11 @@ fun HomeHostScreen() {
         NavHost(
             navController = navController,
             startDestination = NavigationGraphs.HOME_GRAPH_ROUTE,
-            modifier = Modifier.padding(it)
+            modifier = Modifier.padding(it),
+            enterTransition = { fadeIn(tween(350)) },
+            exitTransition = { fadeOut(tween(350)) },
+            popEnterTransition = { fadeIn(tween(350)) },
+            popExitTransition = { fadeOut(tween(350)) }
         ) {
             homeGraph(navController)
         }
