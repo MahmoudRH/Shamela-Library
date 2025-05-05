ac9632112c0152e7d2ec3d9d432954e83c095d7d
Mahmoud Habib <mhmoudrhabib@gmail.com>
2025-05-06 01:02:56 +0300

    Refactor: Improve DefaultTopBar composable
    - Made `onActionClick` and `onNavigateBack` optional parameters with default values.
    - Added `let` to `onNavigateBack` to make the code cleaner.
    - Added preview composables for `DefaultTopBar` to make it easier to test.
    - Used `AutoMirrored.Filled.ArrowBackIos` icon instead of `ArrowForwardIos` for back navigation.
    - Changed `actionIcon` default value to `null`.
    - Added `stringResource` for default top bar preview.
    
    Signed-off-by: Mahmoud Habib <mhmoudrhabib@gmail.com>

--- apptheme/src/main/java/com/shamela/apptheme/presentation/common/TopBar.kt
+++ apptheme/src/main/java/com/shamela/apptheme/presentation/common/TopBar.kt
@@ -2,7 +2,8 @@ package com.shamela.apptheme.presentation.common
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,17 +14,21 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.shamela.apptheme.R
 import com.shamela.apptheme.presentation.theme.AppFonts
+import com.shamela.apptheme.presentation.theme.AppTheme
+import com.shamela.apptheme.presentation.util.ShamelaPrev
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultTopBar(
     title: String,
     actionIcon: ImageVector? = null,
-    onActionClick: () -> Unit = {},
+    onActionClick: (() -> Unit) = {},
+    onNavigateBack: (() -> Unit)? = null,
 ) {
     CenterAlignedTopAppBar(
         modifier = Modifier,
@@ -39,56 +44,41 @@ fun DefaultTopBar(
                     }
                 }
             }
-        }
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DefaultTopBar(
-    title: String,
-    actionIcon: ImageVector? = null,
-    onActionClick: () -> Unit = {},
-    onNavigateBack: () -> Unit,
-) {
-    CenterAlignedTopAppBar(
-        modifier = Modifier,
-        title = { Text(text = title, style = AppFonts.textLargeBold) },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(15.dp),
-        ),
-        actions = {
-            AnimatedVisibility(visible = actionIcon != null) {
-                IconButton(onClick = onActionClick) {
-                    Icon(actionIcon!!, contentDescription = null)
-                }
-            }
         },
         navigationIcon = {
-            IconButton(onClick = onNavigateBack) {
-                Icon(Icons.Default.ArrowForwardIos, contentDescription = null)
+            onNavigateBack?.let {
+                IconButton(onClick = onNavigateBack) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBackIos, contentDescription = null)
+                }
             }
         }
     )
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
+@ShamelaPrev
 @Composable
-fun DefaultTopBar(
-    title: String,
-    onNavigateBack: () -> Unit,
-) {
-    CenterAlignedTopAppBar(
-        modifier = Modifier.shadow(elevation = 4.dp),
-        title = { Text(text = title, style = AppFonts.textLargeBold) },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.surface,
-        ),
-        navigationIcon = {
-            IconButton(onClick = onNavigateBack) {
-                Icon(Icons.Default.ArrowForwardIos, contentDescription = null)
-            }
-        }
-    )
+private fun DefaultTopBarPrev() {
+    AppTheme.ShamelaLibraryTheme {
+        DefaultTopBar(stringResource(R.string.normal))
+    }
+}
+
+@ShamelaPrev
+@Composable
+private fun DefaultTopBar2Prev() {
+    AppTheme.ShamelaLibraryTheme {
+        DefaultTopBar(stringResource(R.string.normal), onNavigateBack = {})
+    }
+}
+
+@ShamelaPrev
+@Composable
+private fun DefaultTopBar3Prev() {
+    AppTheme.ShamelaLibraryTheme {
+        DefaultTopBar(
+            stringResource(R.string.normal),
+            onNavigateBack = {},
+            actionIcon = Icons.Default.Search
+        )
+    }
 }
