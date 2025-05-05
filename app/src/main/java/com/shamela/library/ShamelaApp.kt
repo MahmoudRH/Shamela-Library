3201f435420dac240899876430a40e9ad7fc5dc2
Mahmoud Habib <mhmoudrhabib@gmail.com>
2025-05-06 02:21:41 +0300

    feat: Setup notification channels
    - Created notification channels for each `ChannelType` on app startup.
    - Utilized `NotificationHelper` to manage channel creation.
    - Ensured channels are set up when the app targets API level 26 or higher.
    
    Signed-off-by: Mahmoud Habib <mhmoudrhabib@gmail.com>

--- app/src/main/java/com/shamela/library/ShamelaApp.kt
+++ app/src/main/java/com/shamela/library/ShamelaApp.kt
@@ -1,11 +1,14 @@
 package com.shamela.library
 
 import android.app.Application
+import android.content.Context
 import com.shamela.apptheme.data.db.DatabaseHelper
 import com.shamela.apptheme.data.sharedPrefs.SharedPreferencesData
 import com.shamela.apptheme.presentation.theme.AppFonts
 import com.shamela.apptheme.presentation.theme.AppTheme
 import com.shamela.apptheme.presentation.theme.colors.AppColors
+import com.shamela.apptheme.presentation.util.notifications.ChannelType
+import com.shamela.apptheme.presentation.util.notifications.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 import java.io.File
 
@@ -39,5 +42,10 @@ class ShamelaApp : Application() {
             saveAvailableFontSizes(availableFontSizes)
             saveAvailableColorSchemes(availableColorSchemes)
         }
+        setupNotificationChannels(this)
+    }
+
+    private fun setupNotificationChannels(context: Context) {
+        ChannelType.entries.forEach { NotificationHelper.createChannel(context, it) }
     }
 }
