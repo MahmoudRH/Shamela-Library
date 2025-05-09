51eafb9ebd888b291c1bc8eab19b9dcbbbd12c66
Mahmoud Habib <mhmoudrhabib@gmail.com>
2025-05-10 01:32:38 +0300

    Refactor: Optimize book preparation worker
    - Introduced bulk insertion of book pages using `database.insertBookPages` instead of individual insertions in a loop.
    - Measured the time taken for reading and inserting book pages for performance analysis.
    - Simplified the process of obtaining book title and category.
    - Improved the handling of EPUB parsing and ZIP file reading by adding logging for errors.
    - Used `measureTime` for timing book page insertion.
    
    Signed-off-by: Mahmoud Habib <mhmoudrhabib@gmail.com>

--- apptheme/src/main/java/com/shamela/apptheme/presentation/worker/BookPreparationWorker.kt
+++ apptheme/src/main/java/com/shamela/apptheme/presentation/worker/BookPreparationWorker.kt
@@ -1,5 +1,6 @@
 package com.shamela.apptheme.presentation.worker
 
+import android.app.Notification
 import android.content.Context
 import android.content.pm.ServiceInfo
 import android.os.Build
@@ -7,6 +8,7 @@ import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
+import com.shamela.apptheme.R
 import com.shamela.apptheme.data.db.DatabaseHelper
 import com.shamela.apptheme.data.util.ArabicNormalizer
 import com.shamela.apptheme.domain.model.BookPage
@@ -15,98 +17,124 @@ import com.shamela.apptheme.presentation.util.notifications.NotificationHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.jsoup.parser.Parser.xmlParser
 import org.readium.r2.streamer.parser.EpubParser
 import java.io.File
 import java.util.UUID
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.measureTime
 
 
 class BookPreparationWorker(
     private val appContext: Context,
     private val params: WorkerParameters,
-) :
-    CoroutineWorker(appContext, params) {
+) : CoroutineWorker(appContext, params) {
 
     override suspend fun doWork(): Result {
+        val notificationId = id.hashCode()
         val notification = NotificationHelper.startNotification(
-            context = appContext, title = "تحضير الكتب للبحث",
+            context = appContext,
             type = ChannelType.BookPreparation,
-            content = "يتم تحضير الكتب للبحث"
+            title = appContext.getString(R.string.book_preparation_notification_title),
+            content = appContext.getString(R.string.book_preparation_notification_content)
         )
-        val notificationId = this.id.hashCode()
-        val foreground = if (Build.VERSION.SDK_INT >= 34) {
-            ForegroundInfo(notificationId, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
-        } else {
-            ForegroundInfo(notificationId, notification)
-        }
-        setForegroundAsync(foreground)
+
+        setForegroundAsync(createForegroundInfo(notificationId, notification))
 
         return withContext(Dispatchers.IO) {
             val bookFilePath = params.inputData.getString(EPUB_FILE_PATH)
                 ?: return@withContext Result.failure()
-            val database = DatabaseHelper(appContext)
-            getPages(bookFilePath).forEach { (href, pageData) ->
-                val tokens = bookFilePath.split("/")
-                val bookTitle = tokens[tokens.lastIndex].removeSuffix(".epub")
-                val category = tokens[tokens.lastIndex - 1]
-                val uuidName = bookTitle + category
-                val bookID = UUID.nameUUIDFromBytes(uuidName.toByteArray()).toString()
-                val page = BookPage(
-                    href = href,
-                    content = pageData,
-                    bookId = bookID,
-                    category = category,
-                    bookTitle = bookTitle
-                )
 
-                val isSuccess = database.insertBookPage(page)
-                Log.e(TAG, "doWork: pageInserted $isSuccess")
-
+            var readPagesTime = System.currentTimeMillis()
+            val pages = getPages(bookFilePath)
+            readPagesTime = System.currentTimeMillis() - readPagesTime
+            Log.e(TAG, "readPagesTime = ${readPagesTime.milliseconds}")
+            val bookInsertionTime = measureTime {
+                insertBookPages(bookFilePath, pages)
             }
-            database.close()
-            return@withContext Result.success()
+            Log.e(TAG, "bookInsertionTime = $bookInsertionTime")
+            Result.success()
         }
+    }
+
+    private fun createForegroundInfo(id: Int, notification: Notification): ForegroundInfo {
+        return if (Build.VERSION.SDK_INT >= 34) {
+            ForegroundInfo(id, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        } else {
+            ForegroundInfo(id, notification)
+        }
+    }
+
+    private suspend fun insertBookPages(bookFilePath: String, pages: Map<String, String>) {
+        val database = DatabaseHelper(appContext)
+        val bookFile = File(bookFilePath)
+        val bookTitle = bookFile.name.removeSuffix(".epub")
+        val category = bookFile.parentFile?.name ?: "unknown"
+        val bookID = UUID.nameUUIDFromBytes((bookTitle + category).toByteArray()).toString()
+
+        var pageMappingTime = System.currentTimeMillis()
+        val pagesList = pages.map { (href, content) ->
+            BookPage(
+                href = href,
+                content = content,
+                bookId = bookID,
+                category = category,
+                bookTitle = bookTitle
+            )
+        }
+        pageMappingTime = System.currentTimeMillis() - pageMappingTime
+        Log.e(
+            TAG,
+            "pageMappingTime = ${pageMappingTime.milliseconds}, pageListSize = ${pagesList.size}"
+        )
+
+        database.insertBookPages(pagesList)
+
+        database.close()
     }
 
     private suspend fun getPages(bookFilePath: String): Map<String, String> {
         val normalizer = ArabicNormalizer()
+
         return withContext(Dispatchers.IO) {
             val hrefs = try {
-                EpubParser().parse(bookFilePath)?.publication?.readingOrder?.mapNotNull { it.href }
+                EpubParser().parse(bookFilePath)
+                    ?.publication
+                    ?.readingOrder
+                    ?.mapNotNull { it.href }
                     ?: emptyList()
-            }catch (e: Exception){
-                Log.e("BookPreparationWorker", "parseEpub: ${e.message}")
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to parse EPUB: ${e.message}")
                 emptyList()
             }
+
+            val pagesMap = mutableMapOf<String, String>()
             try {
-                val pagesMap = mutableMapOf<String, String>()
                 ZipFile(File(bookFilePath)).use { zipFile ->
                     hrefs.forEach { href ->
-                        val entry = ZipEntry(href.removePrefix("/"))
-                        Log.e(TAG, "readPages: href ${href.removePrefix("/")}")
-                        if (entry.name.endsWith(".xhtml")) {
+                        val entryName = href.removePrefix("/")
+                        if (entryName.endsWith(".xhtml")) {
+                            val entry = ZipEntry(entryName)
                             zipFile.getInputStream(entry).use { inputStream ->
-                                val document: Document =
-                                    Jsoup.parse(inputStream, "UTF-8", "", xmlParser())
-                                val pageTextNormalized = normalizer.normalize( document.text())
-                                pagesMap.put(href, pageTextNormalized)
+                                val document = Jsoup.parse(inputStream, "UTF-8", "", xmlParser())
+                                val normalizedText = normalizer.normalize(document.text())
+                                pagesMap[href] = normalizedText
                             }
                         }
                     }
                 }
-                return@withContext pagesMap
             } catch (e: Exception) {
-                e.printStackTrace()
-                return@withContext emptyMap()
+                Log.e(TAG, "Error reading ZIP: ${e.message}")
             }
+
+            pagesMap
         }
     }
 
     companion object {
-        const val TAG = "PrepareBookForSearchService"
+        private const val TAG = "BookPreparationWorker"
         const val EPUB_FILE_PATH = "EPUB_FILE_PATH"
     }
 }
